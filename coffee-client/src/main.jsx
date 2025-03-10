17def9925c47725820c9659f3efb9574dd474603
antnose <antnose28@gmail.com>
2025-03-10 22:11:46 +0600

    complete 2nd video of module 56

--- coffee-client/src/main.jsx
+++ coffee-client/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AddCoffee from "./components/AddCoffee/AddCoffee.jsx";
+import UpdateCoffee from "./components/UpdateCoffee/UpdateCoffee.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "addCoffee",
+    element: <AddCoffee />,
+  },
+  {
+    path: "updateCoffee",
+    element: <UpdateCoffee />,
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -7,6 +8,42 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Code Start
+// userName: coffee
+// pass: pyd9tvtK4TjQafAN
+
+// const uri =
+// "mongodb+srv://<username>:<password>@cluster0.to58y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri =
+  "mongodb+srv://coffee:pyd9tvtK4TjQafAN@cluster0.to58y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// MongoDB Code End
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
@@ -14,4 +51,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
 });
-// 12.01
